82145905c398d04ede2460a00c5cc72bc8919840
SaitovErik <saitoverik@mail.ru>
2022-04-09 16:34:16 +0400

    Добавление расчётного класса для сервлета

--- src/main/java/LR4/Calc.java
+++ src/main/java/LR4/Calc.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Calc", urlPatterns="/JavaCalc") 
+@WebServlet(name="Calc", urlPatterns="/JavaCalc") //
 public class Calc extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,34 +22,48 @@ public class Calc extends HttpServlet {
 	private static class RequestCalc {
 		private final String first_calc;
 		private final String second_calc;
+		private final String third_calc;
+		private final String four_calc;
 		private int result;
 						
-		private RequestCalc (String first, String second) {
+		private RequestCalc (String first, String second, String third, String four) {
 			this.first_calc = first;
 			this.second_calc = second;
+			this.third_calc = third;
+			this.four_calc = four;
 			}
 		
 		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
 			return new RequestCalc(
-			request.getParameter("first"),
-			request.getParameter("second"));
+			request.getParameter("first_side"),
+			request.getParameter("second_side"), 
+			request.getParameter("third_side"), 
+			request.getParameter("fourth_side"));
 			}
 				
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
-			request.setAttribute("first_result", first_calc);
-			request.setAttribute("second_result", second_calc);
+			request.setAttribute("first_side_result", first_calc);
+			request.setAttribute("second_side_result", second_calc);
+			request.setAttribute("third_side_result", third_calc);
+			request.setAttribute("fourth_side_result", four_calc);
 			int first_try;
 			int second_try;
+			int third_try;
+			int four_try;
 			try { 
 			first_try=Integer.parseInt(first_calc);
 			second_try=Integer.parseInt(second_calc);
+			third_try=Integer.parseInt(third_calc);
+			four_try=Integer.parseInt(four_calc);
 			}
 			catch (NumberFormatException e) {
 				first_try=0;
-				second_try=0;	
+				second_try=0;
+				third_try=0;
+				four_try=0;
 			}
 			
-			result=first_try+second_try;
+			result=first_try + second_try + third_try=0; + four_try=0;;
 			request.setAttribute("result", result);
 		}
 		
