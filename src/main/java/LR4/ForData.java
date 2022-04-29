576b1ab8e5a0fad9481bbc5acda7d018fc6011aa
Tskrialashvili Diana u2 <dianulya2010@yandex.ru>
2022-04-29 19:00:42 +0500

    Создание абстрактного класса

---
+++ src/main/java/LR4/ForData.java
@@ -0,0 +1,19 @@
+package LR4;
+
+public abstract class ForData {
+	
+	public abstract void setSides(double first_side, double second_side, double third_side, double fourth_side);
+	
+	public abstract double getFirst();
+	
+	public abstract double getSecond();
+	
+	public abstract double getThird();
+	
+	public abstract double getFourth();
+	
+	public abstract double getResult();
+	
+	public abstract double result();
+	
+}
